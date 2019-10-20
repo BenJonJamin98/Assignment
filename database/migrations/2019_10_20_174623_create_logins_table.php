de35e7f32e73d805e2f4c6498447be0ade87c070
BenJonJamin98 <robo1998123@hotmail.co.uk>
2019-10-20 20:39:07 +0100

    Update to logins table creation which adds an email option

--- database/migrations/2019_10_20_174623_create_logins_table.php
+++ database/migrations/2019_10_20_174623_create_logins_table.php
@@ -1,9 +1,7 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 class CreateLoginsTable extends Migration
 {
     /**
@@ -17,11 +15,11 @@ class CreateLoginsTable extends Migration
             $table->increments('id');
             $table->string('username');
             $table->string('password');
+            $table->string('email');
             $table->unsignedTinyInteger('role');
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
