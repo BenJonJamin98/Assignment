1f5063d1d623f4d54b39ce6cd5ccc6f0d35022d4
BenJonJamin98 <robo1998123@hotmail.co.uk>
2019-11-11 16:35:17 +0000

    Login out put views

---
+++ resources/views/login/auth.blade.php
@@ -0,0 +1,25 @@
+{{--@extends('layout.master')--}}
+{{--@section('content')--}}
+{{--<header><h2>Already a member?<br> Sign in here</h2></header>--}}
+
+{{--<div class="container">--}}
+
+{{--    <form method="POST" action="{{ route('login') }}">--}}
+{{--        @csrf--}}
+{{--        <label for="email">Enter Email</label>--}}
+{{--        <input id="email" type="email" class="form-control @error('error') is-invalid @enderror"--}}
+{{--               name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>--}}
+{{--        <label for="password">Enter Password</label>--}}
+{{--        <input id="password" type="password"--}}
+{{--               class="form-control @error('error') is-invalid @enderror" name="password" required--}}
+{{--               autocomplete="current-password">--}}
+{{--        <button type="submit" class="btn btn-primary">--}}
+{{--            {{ __('Login') }}--}}
+{{--        </button>--}}
+{{--    </form>--}}
+
+{{--</div>--}}
+{{--@endsection--}}
+
+{{--@push('scripts')--}}
+{{--@endpush--}}
