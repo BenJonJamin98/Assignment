71ce371d9e02c0e8f61f3534de5425ae6f44ca64
BenJonJamin98 <robo1998123@hotmail.co.uk>
2019-11-25 11:51:18 +0000

    view a comment

--- resources/views/comment/show.blade.php
+++ resources/views/comment/show.blade.php
@@ -1,1 +1,28 @@
-<?php
+@extends('setup')
+@section('page_title')
+    Guestbook | Comment from {{ $comment -> user }}
+@endsection
+@section('page_heading')
+    <h2>{{$comment -> user }} comments</h2>
+@endsection
+@section('content')
+    <table>
+        <tbody>
+{{--            <tr>--}}
+{{--                <td>Name: </td>--}}
+{{--                <td>{{ $comment -> user }}</td>--}}
+{{--            </tr>--}}
+        <tr>
+            <td><h4>Comment: </h4></td>
+            <td><p>{{ $comment -> comments }}</p></td>
+        </tr>
+        <tr>
+            <td><h6>Date: </h6></td>
+            <td><h6>{{ $comment -> created_at -> format ('l jS F') }}</h6></td>
+        </tr>
+        </tbody>
+    </table>
+    <div class="back-button">
+        <p><a class="button" href="/">Back</a></p>
+    </div>
+@endsection
